11aadb334a69e83d25ac8b9225ec7e4f9b808aa6
mpixton <71086131+mpixton@users.noreply.github.com>
2021-04-08 12:45:56 -0600

    FEAT: add data access layer
    Added Bare Data Access layer files.

---
+++ DAL/FagElGamousDbCOntext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using INTEX2.Models;
+
+#nullable disable
+
+namespace INTEX2.DAL
+{
+    public partial class FagElGamousDbCOntext : DbContext
+    {
+        public FagElGamousDbCOntext()
+        {
+        }
+
+        public FagElGamousDbCOntext(DbContextOptions<FagElGamousDbCOntext> options)
+            : base(options)
+        { }
+    }
+}
---
+++ DAL/IUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using INTEX2.DAL;
+using INTEX2.Models;
+
+namespace INTEX2.DAL
+{
+    /// <summary>
+    /// Defines the members for the Unit Of Work class. 
+    /// </summary>
+    public interface IUnitOfWork
+    {
+        // All Generic Repos for the project.
+        /// <summary>
+        /// Generic Repo for Bowlers.
+        /// </summary>
+
+        /// <summary>
+        /// Saves the changes made to the Repos in this Unit Of Work to the Db.
+        /// </summary>
+        void Save();
+    }
+}
